e3eda299780f0a8e3c4a761d063ce12c5cea0d69
Sergey Abu <dev@iteelion.ru>
2022-05-27 07:07:05 +0300

    Bringing to standards

--- Main/FsHelper.php
+++ Main/FsHelper.php
@@ -8,28 +8,28 @@ class FsHelper
 {
 
     // recursive directory deleting
-    public static function rmdirEX($d)
+    public static function rmDirEx(string $dirPath): bool
     {
-        if (!file_exists($d)) return true;
-        if (!is_dir($d)) return unlink($d);
-        foreach (scandir($d, SCANDIR_SORT_NONE) as $i) {
+        if (!file_exists($dirPath)) return true;
+        if (!is_dir($dirPath)) return unlink($dirPath);
+        foreach (scandir($dirPath, SCANDIR_SORT_NONE) as $i) {
             if ($i == '.' || $i == '..') continue;
-            if (!rmdirEX($d . DIRECTORY_SEPARATOR . $i)) return false;
+            if (!rmdirEX($dirPath . DIRECTORY_SEPARATOR . $i)) return false;
         }
-        return rmdir($d);
+        return rmdir($dirPath);
     }
 
-    public static function rmdirr($d)
+    public static function rmDirR(string $dirPath): bool
     {
-        self::rmdirEX($d);
+        self::rmdirEX($dirPath);
     }
 
     //show file in viewcontent pos
-    public static function AddViewContentFromFile($spot, $file, $pos): void
+    public static function addViewContentFromFile($spot, $filePath, $pos): void
     {
         global $APPLICATION;
         ob_start();
-        include($file);
+        include($filePath);
         $content = ob_get_contents();
         ob_end_clean();
         $APPLICATION->AddViewContent($spot, $content, $pos);
--- Main/Helper.php
+++ Main/Helper.php
@@ -6,51 +6,52 @@ namespace App\Main;
 class Helper
 {
 
-    public static function setTitle($title)
+    // Set title with prefix or suffix (change right here!)
+    public static function setTitle(string $title): void
     {
-        $newTitle = $title . ' (iteelion.ru)';
+        $newTitle = 'prefix ' . $title . ' suffix';
         $GLOBALS['APPLICATION']->SetTitle($newTitle);
     }
 
     // vardump with <pre>
-    public static function pre($v): void
+    public static function pre($var): void
     {
         echo '<pre>';
-        var_dump($v);
+        var_dump($var);
         echo '</pre>';
     }
 
     // show content only for admin
-    public static function admpre($v): void
+    public static function admPre($var): void
     {
         if ($GLOBALS['USER']->IsAdmin()) {
-            self::pre($v);
+            self::pre($var);
         }
     }
 
     // dump var to file
-    public static function dump2file($v, $f): void
+    public static function dump2file($var, $filePath): void
     {
         ob_start();
         var_dump($v);
         $ob = ob_get_clean();
-        file_put_contents($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . $f . '.log', $ob, FILE_APPEND);
+        file_put_contents($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . $filePath . '.log', $ob, FILE_APPEND);
     }
 
-    public static function d2f($v, $f): void
+    public static function d2f($var, $filePath): void
     {
-        self::dump2file($v, $f);
+        self::dump2file($var, $filePath);
     }
 
     // log row to print
-    public static function log2print($str): void
+    public static function log2print(string $str): void
     {
         $row = '[' . date('Y-m-d H:i:s') . '] ' . $str . '<br>';
         echo $row;
     }
 
     // log row to file
-    public static function log2file($str, $logname = 'common'): void
+    public static function log2file(string $str, string $logname = 'common'): void
     {
         $row = '[' . date('Y-m-d H:i:s') . '] ' . $str . PHP_EOL;
         $logPath = $_SERVER['DOCUMENT_ROOT'] . '/local/logs/' . $logname . '.log';
@@ -61,21 +62,21 @@ class Helper
     }
 
     // do log row
-    public static function log($str, $params = []): void
+    public static function log(string $str, array $arParams = []): void
     {
-        if (!$params['logname']) {
-            $params['logname'] = 'common';
+        if (!$arParams['logname']) {
+            $arParams['logname'] = 'common';
         }
-        switch ($params['dst']) {
+        switch ($arParams['dst']) {
             case 'out':
                 self::log2print($str);
                 break;
             case 'file':
-                self::log2file($str, $params['logname']);
+                self::log2file($str, $arParams['logname']);
                 break;
             default:
                 self::log2print($str);
-                self::log2file($str, $params['logname']);
+                self::log2file($str, $arParams['logname']);
         }
     }
 
